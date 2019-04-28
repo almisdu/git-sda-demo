7483bc1bb7538499fc94086e6e93924dc68403a9
almisdu <almisdu@gmail.com>
2019-04-28 11:23:41 +0300

    add comment

---
+++ cmdprg.java
@@ -0,0 +1,9 @@
+public class cmdprg{
+	public static void main(String[] args){
+		System.out.println("Hello, " + args[0] + "!");
+		//for( int i = 0; i < args.length; i++){
+		//	System.out.println("Hello, " + args[i] +"!" );
+
+	//	}
+	}
+}
